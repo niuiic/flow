53502f2d01eebd0ef7aa29ae7e485b6e1d07af32
niuiic <niuiicde@gmail.com>
2023-04-06 22:18:36 +0800

    chore(eslint): allow any

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,17 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  root: true
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname
+  },
+  root: true,
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
 }
