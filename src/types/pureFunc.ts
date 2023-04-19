76af9e84abcf211b01b56ada9783810f7240c220
niuiic <niuiicde@gmail.com>
2023-04-20 00:12:52 +0800

    perf(types): rewrite types

--- src/types/pureFunc.ts
+++ src/types/pureFunc.ts
@@ -1,14 +1,15 @@
-export type AnyObject = Record<string | number | symbol, any>
+export type AnyObject = Record<string, any>
 export type EmptyObject = Record<string | number | symbol, never>
 export type AnyFunction = (...args: any[]) => any
 export type IsAny<T> = 1 extends T & 2 ? true : false
+export type IsNever<T> = [T] extends [never] ? true : false
 
 export type Expand<T> = T extends T
   ? T extends (...args: infer A) => Promise<infer R>
     ? (...args: Expand<A>) => Promise<Expand<R>>
     : T extends (...args: infer A) => infer R
     ? (...args: Expand<A>) => Expand<R>
-    : T extends AnyObject
+    : T extends object
     ? {
         [K in keyof T]: Expand<T[K]>
       }
