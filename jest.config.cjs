7b4a906b897d50477fbe48c1a56829ff096d499c
niuiic <niuiicde@gmail.com>
2023-04-28 13:38:08 +0800

    chore(test): add ts-jest

---
+++ jest.config.cjs
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [require('path').join(__dirname, 'tests/**/*.ts')]
+}
