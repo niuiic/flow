bf8bb21bec8bdac03d0ea3897838eeaeba4498f5
niuiic <niuiicde@gmail.com>
2023-04-29 22:27:26 +0800

    chore(jest): fix config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  testMatch: [require('path').join(__dirname, 'tests/**/*.ts')]
+  testMatch: [require('path').join(__dirname, 'tests/**/*.ts')],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^src/(.+)\\.js$': '<rootDir>/src/$1',
+    '^(.+)\\.js$': '$1'
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true
+      }
+    ]
+  }
 }
