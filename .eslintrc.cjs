130adf83f5783d6633666e9d627cf42d15dc9430
niuiic <niuiicde@gmail.com>
2023-04-30 14:27:09 +0800

    chore(eslint): ignore some rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,9 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-argument': 'off',
-    '@typescript-eslint/require-await': 'off'
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-misused-promises': 'off'
   }
 }
