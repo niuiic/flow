0c467f5553da5058a9e408275d7d8873886b3419
niuiic <niuiicde@gmail.com>
2023-05-21 08:12:24 +0800

    test(delay): shorten test time to fix ci timeout problem

--- tests/functions/strict/delay.spec.ts
+++ tests/functions/strict/delay.spec.ts
@@ -3,14 +3,14 @@ import { delay } from 'src/functions/strict/delay.js'
 describe('delay', () => {
   it('should delay "value" by "wait" time', () => {
     let res = 0
-    delay(100, 1).then((v) => {
+    delay(2, 1).then((v) => {
       res = v
     })
     setTimeout(() => {
       expect(res).toEqual(0)
-    }, 10)
+    }, 1)
     setTimeout(() => {
       expect(res).toEqual(1)
-    }, 200)
+    }, 3)
   })
 })
