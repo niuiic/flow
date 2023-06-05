0e49e6183450bf88c9ee1e34031d88211701d9c3
niuiic <niuiicde@gmail.com>
2023-06-05 18:41:51 +0800

    perf(tap): add type constraints for args of `fn`

--- src/functions/strict/tap.ts
+++ src/functions/strict/tap.ts
@@ -1,28 +1,30 @@
-import { FixedPromise } from 'src/types/basic.js'
+import { FixedPromise, MaybePromise } from 'src/types/basic.js'
+import { Immutable } from 'src/types/immutable.js'
 import { isPromise } from 'util/types'
 
 /**
- * Used to invoke an interceptor and return the original value
+ * Invoke an interceptor and return the original value.
  *
  * @example
  * ```ts
- * tap(console.log, [1, 2, 3])
- * // log [1, 2, 3]
- * // return [1, 2, 3]
+ * const res = tap(console.log, { a: 'a' })
+ * // log {a: 'a'}
+ * // res = {a: 'a'}
+ * const res2 = tap((v) => {v.a = 'b'}, { a: 'a' })
+ * // res2 = {a: 'b'}
  * ```
  *
  * {@link #Repo/tests/functions/strict/tap.spec.ts | More examples}
  */
-function tap<A, R>(fn: (args: Awaited<A>) => R, args: A): A extends Promise<unknown> ? FixedPromise<A> : A
-
-function tap<A, R>(fn: (args: Awaited<A>) => R): (args: A) => A extends Promise<unknown> ? FixedPromise<A> : A
+function tap<A, R>(fn: (args: Immutable<Awaited<A>>) => R, args: A): A extends Promise<unknown> ? FixedPromise<A> : A
 
 function tap<A, R>(
-  fn: (args: Awaited<A>) => R,
-  args?: A
-): FixedPromise<A> | A | ((args: A) => A extends Promise<unknown> ? FixedPromise<A> : A) {
+  fn: (args: Immutable<Awaited<A>>) => R
+): (args: A) => A extends Promise<unknown> ? FixedPromise<A> : A
+
+function tap<A, R>(fn: (args: Immutable<Awaited<A>>) => R, args?: A): MaybePromise<A> | ((args: A) => MaybePromise<A>) {
   if (args === undefined) {
-    return (v: A) => tap(fn, v)
+    return (args: A) => tap(fn, args)
   }
 
   const res = isPromise(args) ? args.then(fn as any) : fn(args as any)
--- tests/functions/strict/tap.spec.ts
+++ tests/functions/strict/tap.spec.ts
@@ -6,6 +6,10 @@ describe('tap', () => {
       expect(tap(console.log, [1, 2, 3])).toEqual([1, 2, 3])
     })
 
+    it('should return a function if "args" is not passed', () => {
+      expect(typeof tap(console.log)).toEqual('function')
+    })
+
     it('should work for async value', () => {
       const value = Promise.resolve(1)
       expect(tap(console.log, value)).toEqual(value)
