4057ac1af997ba0f3f75d758af921f049eee2360
niuiic <niuiicde@gmail.com>
2023-06-20 21:59:40 +0800

    chore(rollup): update config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,15 +1,26 @@
 import typescript from '@rollup/plugin-typescript'
+import path from 'path'
+import { defineConfig } from 'rollup'
+import pkg from './package.json' assert { type: 'json' }
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    dir: './dist',
-    format: 'cjs'
-  },
-  plugins: [
-    typescript({
-      sourceMap: false,
-      exclude: ['./tests/**/*.spec.ts', './cli/**/*.ts']
-    })
-  ]
-}
+export default defineConfig([
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: path.dirname(pkg.module),
+      name: pkg.name,
+      format: 'es',
+      sourcemap: true,
+      exports: 'named',
+      preserveModules: true,
+      preserveModulesRoot: 'src'
+    },
+    plugins: [
+      typescript({
+        outDir: path.dirname(pkg.module),
+        declarationDir: path.dirname(pkg.module),
+        exclude: ['./tests/**/*.spec.ts', './cli/**/*.ts']
+      })
+    ]
+  }
+])
