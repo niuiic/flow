5aa1c5854efdc3d53ec02baf296fc681bea7f157
niuiic <niuiicde@gmail.com>
2023-06-21 14:02:56 +0800

    chore(package): add cjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -22,5 +22,24 @@ export default defineConfig([
         exclude: ['./tests/**/*.spec.ts', './cli/**/*.ts']
       })
     ]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: path.dirname(pkg.main),
+      name: pkg.name,
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named',
+      preserveModules: true,
+      preserveModulesRoot: 'src'
+    },
+    plugins: [
+      typescript({
+        outDir: path.dirname(pkg.main),
+        declarationDir: path.dirname(pkg.main),
+        exclude: ['./tests/**/*.spec.ts', './cli/**/*.ts']
+      })
+    ]
   }
 ])
