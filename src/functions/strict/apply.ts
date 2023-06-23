240202ca68e73df3fbe6b6b6ea13e74135074006
niuiic <niuiicde@gmail.com>
2023-06-23 09:49:41 +0800

    feat(functions): apply

---
+++ src/functions/strict/apply.ts
@@ -0,0 +1,28 @@
+import { AnyFunction } from '../../types/basic.js'
+
+/**
+ * Apply function `fn` to the arguments list `args`.
+ *
+ * @example
+ * ```ts
+ * apply(Math.max, [1, 2, 3, 4, 5]); // 5
+ * ```
+ *
+ * {@link #Repo/tests/functions/strict/apply.spec.ts | More examples}
+ */
+function apply<A extends AnyFunction, P extends Parameters<A>>(fn: A, args: P): ReturnType<typeof fn>
+
+function apply<A extends AnyFunction, P extends Parameters<A>>(fn: A): (args: P) => ReturnType<typeof fn>
+
+function apply<A extends AnyFunction, P extends Parameters<A>>(
+  fn: A,
+  args?: P
+): ReturnType<typeof fn> | ((args: P) => ReturnType<typeof fn>) {
+  if (args === undefined) {
+    return (args) => fn(...args)
+  } else {
+    return fn(...args)
+  }
+}
+
+export { apply }
---
+++ tests/functions/strict/apply.spec.ts
@@ -0,0 +1,11 @@
+import { apply } from 'src/functions/strict/apply.js'
+
+describe('apply', () => {
+  it('should return a function if "args" is not passed', () => {
+    expect(typeof apply(console.log)).toEqual('function')
+  })
+
+  it('should apply "fn" to "args"', () => {
+    expect(apply(Math.max, [1, 2, 3, 4, 5])).toEqual(5)
+  })
+})
