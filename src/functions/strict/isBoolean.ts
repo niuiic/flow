8caf6c6f91b6489edae2126d9919fef8bf5dc802
niuiic <niuiicde@gmail.com>
2023-06-23 10:06:23 +0800

    doc(functions): isBoolean

--- src/functions/strict/isBoolean.ts
+++ src/functions/strict/isBoolean.ts
@@ -1,5 +1,16 @@
 import { Include } from '../../types/include.js'
 
+/**
+ * Returns true if `a` is an Array.
+ *
+ * @example
+ * ```ts
+ * isArray([1, 2, 3]); // true
+ * isArray(2); // false
+ * ```
+ *
+ * {@link #Repo/tests/functions/strict/isBoolean.spec.ts | More examples}
+ */
 function isBoolean<A>(args: A): args is Include<A, boolean> {
   return typeof args === 'boolean'
 }
