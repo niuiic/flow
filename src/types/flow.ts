81b6c5e11071af142b400fabe14b3f62b6fe3137
niuiic <niuiicde@gmail.com>
2023-06-29 13:48:29 +0800

    fix(flow): wrong type

--- src/types/flow.ts
+++ src/types/flow.ts
@@ -1,9 +1,9 @@
 import { Tail } from './basic.js'
-import { FixedPromise } from './promise.js'
+import { MaybePromise } from './promise.js'
 
 /** T = \[...Rest, Last\], any one of Rest is async ? FixedPromise<Last> : Last */
 export type FlowReturnValue<T extends unknown[]> = T extends [infer First, infer Second, ...infer Rest]
-  ? First extends Promise<unknown>
-    ? FixedPromise<Tail<T>>
+  ? [First] extends [MaybePromise<unknown>]
+    ? MaybePromise<Tail<T>>
     : FlowReturnValue<[Second, ...Rest]>
   : T[0]
