bee3bad589ab07d40549cd00529fdf6e150e84f6
niuiic <niuiicde@gmail.com>
2023-06-30 17:04:59 +0800

    feat(pipe): identity

---
+++ src/pipe/strict/identity.ts
@@ -0,0 +1,15 @@
+/**
+ * Return the same value as the given argument.
+ *
+ * @example
+ * ```ts
+ * identity(5); // 5
+ * ```
+ *
+ * {@link #Repo/tests/pipe/strict/identity.spec.ts | More examples}
+ */
+function identity<A>(args: A) {
+  return args
+}
+
+export { identity }
