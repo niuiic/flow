f8d7031b40b9bc7e1c90879120339817ba584805
niuiic <niuiicde@gmail.com>
2023-06-30 22:15:55 +0800

    doc(entries): add description

--- src/pipe/lazy/entries.ts
+++ src/pipe/lazy/entries.ts
@@ -1,5 +1,18 @@
 import { AnyObject, Entries } from 'src/types/index.js'
 
+/**
+ * Convert object to iterator.
+ *
+ * @example
+ * ```ts
+ * const iterator =  entries({a: 1, b: 2})
+ * iterator.next().value = ['a', 1]
+ * iterator.next().value = ['b', 2]
+ * iterator.next().done = true
+ * ```
+ *
+ * {@link #Repo/tests/pipe/lazy/entries.spec.ts | More examples}
+ */
 function* entries<A extends AnyObject>(args: A): Generator<Entries<A>, void> {
   for (const k in args) {
     if (Object.prototype.hasOwnProperty.call(args, k)) {
