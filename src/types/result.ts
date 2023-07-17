617fce15bdd7e076cb94ff11e115f39f7699ce43
niuiic <niuiicde@gmail.com>
2023-07-17 14:30:22 +0800

    fix(result type): wrap T with []

--- src/types/result.ts
+++ src/types/result.ts
@@ -1,10 +1,10 @@
 import { Result } from 'src/flow/result.js'
 import { MaybePromise } from './promise.js'
 
-export type ResultReturnValue<T extends MaybePromise<Result<unknown>>> = T extends Promise<Result<Promise<infer R>>>
+export type ResultReturnValue<T extends MaybePromise<Result<unknown>>> = [T] extends [Promise<Result<Promise<infer R>>>]
   ? MaybePromise<Result<Awaited<R>>>
-  : T extends Promise<Result<infer R>>
+  : [T] extends [Promise<Result<infer R>>]
   ? MaybePromise<Result<R>>
-  : T extends Result<Promise<infer R>>
+  : [T] extends [Result<Promise<infer R>>]
   ? MaybePromise<Result<Awaited<R>>>
   : T
