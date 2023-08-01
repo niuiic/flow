d04e1e8ad102047a32a25fddde68ce3510472e05
niuiic <niuiicde@gmail.com>
2023-08-01 09:37:11 +0800

    feat(pipe): includes

---
+++ tests/pipe/strict/includes.spec.ts
@@ -0,0 +1,29 @@
+import { includes, pipe, toAsync } from 'src/index.js'
+
+describe('includes', () => {
+  describe('sync', () => {
+    it('should work for iterable', () => {
+      expect(includes(1, [1, 2, 3])).toEqual(true)
+    })
+
+    it('should return false if no item in iterable', () => {
+      expect(includes(1, [])).toEqual(false)
+    })
+
+    it('should return a function if "iterable" is not passed', () => {
+      expect(typeof includes(1)).toEqual('function')
+    })
+
+    it('should throw an error if "iterable" is not a iterable', () => {
+      expect(() => includes(1, 1 as any)).toThrow()
+    })
+  })
+
+  describe('async', () => {
+    it('should work for async iterable', () => {
+      pipe([1, 2, 3, 4], toAsync, includes(1)).then((res) => {
+        expect(res).toEqual(true)
+      })
+    })
+  })
+})
