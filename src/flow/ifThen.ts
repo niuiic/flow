47b90f727510f9143bd98b12acd163d2e5a352db
niuiic <niuiicde@gmail.com>
2023-08-14 10:00:40 +0800

    fix(ifThen): type error

--- src/flow/ifThen.ts
+++ src/flow/ifThen.ts
@@ -2,6 +2,8 @@ import { MaybePromise } from 'src/types/index.js'
 import { isPromise } from 'src/utils.js'
 import { Result, err, ok } from './result.js'
 
+type IfThenReturnValue<A, R> = R extends Promise<unknown> ? MaybePromise<Result<A>> : Result<A>
+
 /**
  * Call `fn` if `result` is success and `condition` is satisfied.
  *
@@ -15,38 +17,37 @@ import { Result, err, ok } from './result.js'
  * {@link https://github.com/niuiic/flow/blob/main/tests/flow/ifThen.spec.ts | More examples}
  */
 function ifThen<A, B extends A>(
-  condition: (data: A) => data is B,
-  fn: (data: B) => unknown,
+  guard: (data: A) => data is B,
+  fn: (data: B) => Promise<unknown>,
   result: Result<A>
 ): MaybePromise<Result<A>>
+function ifThen<A, B extends A>(guard: (data: A) => data is B, fn: (data: B) => unknown, result: Result<A>): Result<A>
 function ifThen<A>(
-  condition: (data: A) => boolean,
-  fn: (data: A) => unknown,
+  guard: (data: A) => boolean,
+  fn: (data: A) => Promise<unknown>,
   result: Result<A>
 ): MaybePromise<Result<A>>
-function ifThen<A, B extends A>(
-  condition: (data: A) => data is B,
-  fn: (data: B) => unknown
-): (result: Result<A>) => MaybePromise<Result<A>>
-function ifThen<A>(
-  condition: (data: A) => boolean,
-  fn: (data: A) => unknown
-): (result: Result<A>) => MaybePromise<Result<A>>
+function ifThen<A>(guard: (data: A) => boolean, fn: (data: A) => unknown, result: Result<A>): Result<A>
+function ifThen<A, B extends A, R>(
+  guard: (data: A) => data is B,
+  fn: (data: B) => R
+): (result: Result<A>) => IfThenReturnValue<A, R>
+function ifThen<A, R>(guard: (data: A) => boolean, fn: (data: A) => R): (result: Result<A>) => IfThenReturnValue<A, R>
 
 function ifThen<A>(
-  condition: (data: A) => boolean,
+  guard: (data: A) => boolean,
   fn: (data: A) => unknown,
   result?: Result<A>
 ): MaybePromise<Result<A>> | ((result: Result<A>) => MaybePromise<Result<A>>) {
   if (result === undefined) {
-    return (result) => ifThen(condition, fn, result)
+    return (result) => ifThen(guard, fn, result)
   }
 
   if (result.isError()) {
     return err(result.error()!)
   }
 
-  if (condition(result.unwrap())) {
+  if (guard(result.unwrap())) {
     const res = fn(result.unwrap())
     if (isPromise(res)) {
       return res.then(() => ok(result.unwrap()))
