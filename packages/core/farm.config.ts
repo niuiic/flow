0f796cd24364d33b220acfc428d716345603a674
niuiic <niuiicde@gmail.com>
2023-11-17 09:38:17 +0800

    chore: pack in chunks

--- packages/core/farm.config.ts
+++ packages/core/farm.config.ts
@@ -1,6 +1,42 @@
 import type { UserConfig } from '@farmfe/core'
 import dts from '@farmfe/js-plugin-dts'
-import path from 'node:path'
+import { readdirSync, statSync } from 'node:fs'
+import { join } from 'node:path'
+
+const chunks: {
+  name: string
+  test: string[]
+}[] = []
+
+const walkDir = (dirPath: string, cb: (filePath: string) => void) => {
+  const items = readdirSync(dirPath)
+  items.forEach((item) => {
+    const itemPath = join(dirPath, item)
+    const isDir = statSync(itemPath).isDirectory()
+    if (isDir) {
+      walkDir(itemPath, cb)
+    } else {
+      cb(itemPath)
+    }
+  })
+}
+
+const collectChunks = (filePath: string) => {
+  if (filePath.includes('index.ts')) {
+    return
+  }
+  let matchedText = filePath.match(/src\/([^\.]+).+/)
+  if (matchedText) {
+    chunks.push({
+      name: matchedText[1].replace(/\//g, '_'),
+      test: [matchedText[1]]
+    })
+  }
+}
+
+const workDirs = ['src/flow', 'src/pipe'].map((x) => join(process.cwd(), x))
+
+workDirs.forEach((dirPath) => walkDir(dirPath, collectChunks))
 
 const config: UserConfig = {
   compilation: {
@@ -14,16 +50,11 @@ const config: UserConfig = {
     },
     resolve: {
       alias: {
-        '@/': path.join(process.cwd(), 'src')
+        '@/': join(process.cwd(), 'src')
       }
     },
     partialBundling: {
-      enforceResources: [
-        {
-          name: 'index',
-          test: ['.+']
-        }
-      ]
+      enforceResources: chunks
     },
     minify: true,
     sourcemap: true,
