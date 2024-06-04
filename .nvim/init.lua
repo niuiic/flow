151f5f0396e260f6fc0af8ec44190b5d233184d2
niuiic <niuiicde@gmail.com>
2024-06-04 15:57:50 +0800

    chore: update neovim config

--- .nvim/init.lua
+++ .nvim/init.lua
@@ -1,11 +1,3 @@
-require("task")
-
-local rust_analyzer_config = require("lsp/rust_analyzer")
-rust_analyzer_config.root_dir = function()
-	return require("core").file.root_path() .. "/packages/cli"
-end
-require("lspconfig")["rust_analyzer"].setup(rust_analyzer_config)
-
 require("coverage").setup({
 	lang = {
 		javascript = {
